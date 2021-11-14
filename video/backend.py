80d7a99071c0bdaff9dd5436756588f026f6ed38
b3nc301 <bence.fieszl@gmail.com>
2021-11-15 00:12:46 +0100

    backend

--- video/backend.py
+++ video/backend.py
@@ -7,4 +7,6 @@ from yolov5 import detect
 
 detect.run(weights="best.pt", source="../demos/vid.mp4", view_img="True", imgsz=[640,640])
 
-time.sleep(20)
+
+
+#time.sleep(20)
--- video/pysocket.py
+++ video/pysocket.py
@@ -2,33 +2,34 @@ import asyncio
 import websockets
 import subprocess
 
-
-prep = ""
-
-async def echo(websocket, path):
-    async for message in websocket:
-        await websocket.send(message)
-
+data = {"src":"rtsp://localhost/",
+"wg":"best.pt",
+"frames":"3",
+"imgsz":"[640,640]",
+"conf":"0.7",
+"nms":"0.5",
+"max":"10",
+"min":"1.5"}
 
 async def setup(websocket, path):
     mess=await websocket.recv()
-    message=mess.split(";")
-    print (message)
-    
-    if message[0] == "start":
-       global proc
-       proc = subprocess.Popen(["python3", "backend.py"])
-    elif message[0] == "detection":
-        prep = message[1]
-        proc.terminate()
-        proc = subprocess.Popen(["python3", "backend.py"])
-        await websocket.send(message[1])
-    elif message[0] == "status":
-        await websocket.send(prep)
-    elif message[0] == "stop":
-        proc.terminate()
+    if mess.contains(";"):
+        message=mess.split(";")        
+        if message[0] == "start":
+            global proc
+            prep = data["src"] + data["wg"] + data["frames"] + data["imgsz"] + data["conf"] + data["nms"] + data["max"] + data["min"]
+            proc = subprocess.Popen(["python3", "backend.py", prep])
+        elif message[0] == "detection":
+            prep = data["src"] + data["wg"] + data["frames"] + data["imgsz"] + data["conf"] + data["nms"] + data["max"] + data["min"]
+            proc.terminate()
+            proc = subprocess.Popen(["python3", "backend.py", prep])
+            await websocket.send(message[1])
+        elif message[0] == "status":
+            await websocket.send(data)
+        elif message[0] == "stop":
+            proc.terminate()
     else:
-        print ("shit")
+        await websocket.send("ERROR")
 
 async def main():
     async with websockets.serve(setup, "localhost", 8765):
